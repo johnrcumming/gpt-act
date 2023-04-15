ca158c486494e363e4d00e1276c4c5ebc579d9a4
John Cumming <johnrcumming@gmail.com>
2023-04-14 23:08:56 -0400

    add perplexity calculation

--- gpt2act_trainer.py
+++ gpt2act_trainer.py
@@ -2,14 +2,14 @@ import os
 import sys
 import traceback
 from argparse import ArgumentParser
-
+import torch
+from tqdm import tqdm
 
 import transformers
 import datasets
-#from datasets import list_datasets, load_dataset, list_metrics, load_metric, DatasetDict, Dataset
 
 from transformers import GPT2Config
-from transformers import GPT2Tokenizer
+from transformers import GPT2Tokenizer, GPT2TokenizerFast
 from transformers import Trainer, TrainingArguments
 
 from gpt2_act import GPT2ACTLMHeadModel, GPT2ACTConfig
@@ -169,12 +169,62 @@ def train(data_dir, base_logging_dir, checkpoint_dir, dataset_name,
         traceback.print_exc()
         trainer.save_model(os.path.join(training_args.output_dir, 'crash'))
 
+def calculate_perplexity(data_dir='data', dataset_name='wikitext', model_config='gpt2', checkpoint=None, num_procs=4, verbose=True, no_cuda=True):
+    if verbose:
+        transformers.utils.logging.set_verbosity_info()
+
+
+    model = GPT2ACTLMHeadModel.from_pretrained(checkpoint, torch_dtype=torch.float16).to('cpu' if no_cuda else 'cuda')
+
+    dataset_dir=os.path.join(data_dir, dataset_name)
+    dataset = datasets.DatasetDict.load_from_disk(dataset_dir)
+    encodings = dataset['test']
+
+    max_length = model.config.n_positions
+    stride = 512
+    
+    seq_len = len(encodings['input_ids'][0])
+
+
+    nlls = []
+    prev_end_loc = 0
+    for begin_loc in tqdm(range(0, seq_len, stride)):
+        end_loc = min(begin_loc + max_length, seq_len)
+        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+        e = torch.tensor(encodings['input_ids'])
+        input_ids = e[:, begin_loc:end_loc].to('cpu' if no_cuda else 'cuda')
+        target_ids = input_ids.clone()
+        target_ids[:, :-trg_len] = -100
+
+        print('input_ids', input_ids.shape, input_ids.device)
+        print('target_ids', target_ids.shape, target_ids.device)
+
+        with torch.no_grad():
+            outputs = model(input_ids, labels=target_ids)
+
+            # loss is calculated using CrossEntropyLoss which averages over valid labels
+            # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
+            # to the left by 1.
+            neg_log_likelihood = outputs.loss
+
+        nlls.append(neg_log_likelihood)
+
+        prev_end_loc = end_loc
+        if end_loc == seq_len:
+            break
+
+    ppl = torch.exp(torch.stack(nlls).mean())
+
+    print('Perplexity:', ppl)
+
 
 def main():
     parser = ArgumentParser(description='Perform GPT2ACT Dataset Build and Training.', add_help=False)
 
     parser.add_argument('--preprocess_dataset', default=False, action='store_true', help='Build Dataset.')
     parser.add_argument('--train_model', default=False, action='store_true', help='Train Model.')
+    parser.add_argument('--calculate_perplexity', default=False, action='store_true', help='Calculate Perplexity of Trained Model.')
+
 
     parser.add_argument('--model_config', type=str, default="gpt2-xl", help='Huggingface Transformers Model Config.')
     parser.add_argument('--dataset_name', type=str, default="openwebtext", help='Huggingface Datasets Name.')
@@ -229,5 +279,8 @@ def main():
                 no_cuda=args.no_cuda, logging_steps=args.logging_steps, save_steps=args.save_steps,
              )
         
+    if args.calculate_perplexity:
+        calculate_perplexity(data_dir=args.data_dir, dataset_name=args.dataset_name, checkpoint=args.checkpoint, 
+                             num_procs=args.num_procs, verbose=args.verbose, no_cuda=args.no_cuda)
 if __name__ == "__main__":
     main()
