90f382b064e0fd775b5c998d46a9c821f531a571
John Cumming <john@utr.ai>
2023-07-22 15:54:50 -0400

    optimized BinaryPositionEmbeddings

--- embeddings.py
+++ embeddings.py
@@ -1,51 +1,38 @@
 import torch
+import torch.nn as nn
 import math
 
-class BinaryPositionEmbedding(torch.nn.Module):
-    """
-    This module produces binary embeddings given a position index tensor.
-    Binary Embeddings are a set of learnable embeddings that are the sum of a set of binary vectors.
-
-    Args:
-        n_positions (:obj:`int`):  The maximum value of the dimensionality of position embeddings, i.e. :obj:`n_positions` = :obj:`seq_length + 1`.
-        d_model (:obj:`int`):  The hidden size of the embeddings.
-    """
+class BinaryPositionEmbedding(nn.Module):
     def __init__(self, n_positions, d_model):
         super(BinaryPositionEmbedding, self).__init__()
-        
+
         self.n_bits = math.ceil(math.log2(n_positions))
         self.d_model = d_model
-        self.d_length = 2**self.n_bits
-        self.embedding = torch.nn.Embedding(self.n_bits, self.d_model)
-        # Normalize Embedding Parameters
-        self.embedding.weight.data = torch.nn.functional.normalize(self.embedding.weight.data, dim=1, p=2)
+        self.embedding = nn.Embedding(self.n_bits, d_model)
 
-    def embed(self, pos):
-        """
-        Embed a single position index.
-        Args:
-            pos (:obj:`int`):  The position index to embed.
-        Returns:
-            :obj:`torch.Tensor`:  The embedding of the position index :obj:`pos`.
-        """        
-        y = torch.LongTensor([i for i in range(self.n_bits) if torch.bitwise_and(pos, 2**i) != 0]).to(self.embedding.weight.device)
-        y = self.embedding(y)
-        y = torch.sum(y, dim=0)
-        return y
-    
+        # Normalize Embedding Parameters
+        self.embedding.weight.data = F.normalize(self.embedding.weight.data, dim=1, p=2)
+
     def forward(self, x):
-        """
-        Embed a set of position indices.
-        Args:
-            x (:obj:`torch.Tensor`):  The position indices to embed.
-        Returns:
-            :obj:`torch.Tensor`:  The embeddings of the position indices :obj:`x`.
-        """
         x_shape = x.shape
-        x = torch.flatten(x)
-        y = torch.stack([self.embed(i) for i in x])
-        y = y.reshape(*x_shape, self.d_model)
-        return y
+        x = x.flatten()
+
+        # create a tensor with shape (n_bits, x.shape[0])
+        bit_indices = torch.arange(self.n_bits, device=x.device).unsqueeze(1).repeat(1, x.shape[0])
+
+        # create a tensor with shape (n_bits, x.shape[0]) where each column is the binary representation of a position
+        binary_repr = x.unsqueeze(0) & (1 << bit_indices)
+
+        # select the active bits for each position
+        active_bits = torch.nonzero(binary_repr).T[1]
+
+        # calculate embeddings and sum them for each position
+        embeddings = self.embedding(active_bits)
+        embeddings = embeddings.split_with_sizes(x_shape.numel(), dim=0)
+        embeddings = torch.stack([torch.sum(e, dim=0) for e in embeddings])
+
+        embeddings = embeddings.reshape(*x_shape, self.d_model)
+        return embeddings
 
 
 class BinaryRelativePositionEmbedding(torch.nn.Module):
@@ -99,3 +86,11 @@ class RelativePositionEmbedding(torch.nn.Module):
         relative_positions = self._embedding(positions)
         relative_scores = torch.einsum('bhld,md->bhlm', q, relative_positions)
         return relative_scores
+
+
+
+     
+
+
+
+        
