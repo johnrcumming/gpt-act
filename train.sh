604c3617b7c6286113b573bc26d3cc19b6adcbf8
John Cumming <johnrcumming+github@gmail.com>
2025-03-28 21:51:21 -0400

    addad train.sh

---
+++ train.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+python gpt2act_trainer.py --preprocess_dataset \
+    --dataset wikitext
+    
+accelerate launch gpt2act_trainer.py --train_model \
+    --dataset wikitext \
+    --train_batch_size=6 --gradient_accumulation_steps=512 \
+    --run_name gpt2actmoe1 \
+    --logging_steps=5
+
